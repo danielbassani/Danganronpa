322f406f7b17b5c6b390781979da11db953572a7
dantheman0124 <daniel.bassani@outlook.com>
2021-08-02 20:28:04 -0400

    finally figured out how to deal with SKIP properly

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -38,6 +38,9 @@ public class DialogueController : MonoBehaviour
     bool audioPlayed = false;
 	
 	void Start() {
+        /* initialize UI objects by finding them in scene, we will set these to display name of person who is speaking,
+         * dialogue line, and the sprite to display next
+         */
         name = GameObject.Find("Name").GetComponent<Text>();
         textfield = GameObject.Find("Dialogue").GetComponent<Text>();
         sprite = GameObject.Find("Sprite").GetComponent<Image>();
@@ -48,7 +51,7 @@ public class DialogueController : MonoBehaviour
         // if the player clicked on has dialogue enabled
         if (dialogueEnabled)
         {
-            targetTime -= Time.deltaTime;
+            targetTime -= Time.deltaTime; // decrease time between switching to next dialogue line
 
             // if the player is not frozen
             if (!frozen)
@@ -86,14 +89,24 @@ public class DialogueController : MonoBehaviour
                         this.GetComponent<TypeWriterEffect>().fullText = dialogue[0]; // set text in TypeWriterEffect to display
                         this.GetComponent<TypeWriterEffect>().updateText = true; // tell TypeWriterEffect to update the text
                         textUpdated = true; // set to true so that it does not keep updating over and over
-                        sprite.sprite = dialogueSprites[0]; // set sprite 1 to the first sprite
+
+                        // if sprite one is active we want to set it since it is on screen
+                        if (spriteOneActive)
+                        {
+                            sprite.sprite = dialogueSprites[0]; // set sprite 1 to the first sprite
+                        } 
+                        // otherwise that means sprite 2 is active and we want to set it since it is on screen
+                        else
+                        {
+                            sprite2.sprite = dialogueSprites[0]; // set sprite 2 to the first sprite
+                        }
                         index++; // increase index
 
                         // play soundclip if there is one, if audio was not played, and we are NOT supposed to skip current index
                         if (audioClips[index] != null && !audioPlayed)
                         {
-                            audioSource.PlayOneShot(audioClips[index]);
-                            audioPlayed = true;
+                            audioSource.PlayOneShot(audioClips[index]); // play once
+                            audioPlayed = true; // set to true to avoid audio spamming every frame
                         }
                     }
                     else
@@ -106,22 +119,25 @@ public class DialogueController : MonoBehaviour
                         // if sprite 1 is currently showing on screen
                         if (spriteOneActive)
                         {
+                            // only set it if we have a sprite to set it to, otherwise it will stay the same on screen and not switch
                             if (dialogueSprites[index] != null)
                             {
-                                StartCoroutine(FadeOut(sprite));
+                                StartCoroutine(FadeOut(sprite)); // fade out the current sprite
                                 sprite2.sprite = dialogueSprites[index]; // set sprite2 to next sprite
-                                StartCoroutine(FadeIn(sprite2));
+                                StartCoroutine(FadeIn(sprite2)); // fade in new sprite
                                 spriteOneActive = false; // spriteOne is no longer active
                             }
 
                         }
+                        // otherwise sprite two is on screen
                         else
                         {
+                            // only set it if we have a sprite to set it to, otherwise it will stay the same on screen and not switch
                             if (dialogueSprites[index] != null)
                             {
-                                StartCoroutine(FadeOut(sprite2));
+                                StartCoroutine(FadeOut(sprite2)); // fade out current sprite
                                 sprite.sprite = dialogueSprites[index]; // set sprite1 to next sprite
-                                StartCoroutine(FadeIn(sprite));
+                                StartCoroutine(FadeIn(sprite)); // fade in new sprite
                                 spriteOneActive = true; // spriteOne is now active
                             }
 
@@ -130,37 +146,57 @@ public class DialogueController : MonoBehaviour
                         // play soundclip if there is one, if audio was not played, and we are NOT supposed to skip current index
                         if (audioClips[index] != null && !audioPlayed)
                         {
-                            audioSource.PlayOneShot(audioClips[index]);
-                            audioPlayed = true;
+                            audioSource.PlayOneShot(audioClips[index]); // play once
+                            audioPlayed = true; // set to true to avoid audio spamming every frame
                         }
                     }
                 }
 
 
-                // on mouse click left
+                // on mouse click left and if the time between clicks has hit 0
                 if (Input.GetMouseButtonDown(0) && targetTime <= 0f)
                 {
-                    index++; // increase index to go to next name, dialogue, sprite, audio
-                    textUpdated = false; // set to false so we can update name, dialogue, sprite, audio
-                    audioSource.Stop(); // stop current audio clip to avoid overlap
-                    audioPlayed = false; // reset audio played to false so next one can be played
-                    this.GetComponent<TypeWriterEffect>().updateText = false; // set to false
-                    StopCoroutine(this.GetComponent<TypeWriterEffect>().ShowText()); // stop current text scrolling
-                    targetTime = 0.6f; // reset for next name, dialogue, sprite, sound
+                    // if the next dialogue line has not reached the end and if the next dialogue is special "SKIP"
+                    if (index + 1 < dialogue.Length && dialogue[index + 1] == "SKIP")
+                    {
+                        dialoguePanel.GetComponent<PanelFader>().Fade(); // fade the character's dialogue panel
+                        StartCoroutine("Wait"); // set dialogue panel inactive after 0.5f
+                        Invoke("SetToFalse2", 0.7f); // USE SET TO FALSE 2 because it does what SetToFalse does but also updates next sprite
+                        HUD.SetActive(true); // set HUD active
+                        HUD.GetComponent<PanelFader>().FadeIfInitiallyShowing(); // fade back in the HUD
+                        dialogueEnabled = false; // set false now before index so it doesn't show next name, dialogue, sprite, sound
+                        index += 2; // skip over "SKIP"
+                    } else
+                    {
+                        index++; // increase index to go to next name, dialogue, sprite, audio
+                        textUpdated = false; // set to false so we can update name, dialogue, sprite, audio
+                        audioSource.Stop(); // stop current audio clip to avoid overlap
+                        audioPlayed = false; // reset audio played to false so next one can be played
+                        this.GetComponent<TypeWriterEffect>().updateText = false; // set to false
+                        StopCoroutine(this.GetComponent<TypeWriterEffect>().ShowText()); // stop current text scrolling
+                        targetTime = 0.6f; // reset for next name, dialogue, sprite, sound
+                    }
+                    
                 }
             } 
         } else
         {
-            print("no dialogue");
+            // print("no dialogue");
         }
 	}
 
+    /*
+     * This method disables player movement and looking around
+     */
 	public void FreezePlayer() {
         player.GetComponent<PlayerMovement>().enabled = false;
         player.GetComponentInChildren<MouseLook>().enabled = false;
         frozen = true;
 	}
 
+    /*
+     * This method enables player movement and looking around
+     */
     public void UnfreezePlayer()
     {
         player.GetComponent<PlayerMovement>().enabled = true;
@@ -168,22 +204,35 @@ public class DialogueController : MonoBehaviour
         frozen = false;
     }
 
+    /*
+     * This method hides the character you are currently talking to on the field
+     */
     public void HideCharacter()
     {
         character.SetActive(false);
     }
 
+    /*
+     * This method shows the character you are currently talking to on the field
+     */
     public void ShowCharacter()
     {
         character.SetActive(true);
     }
 
+    /*
+     * This method waits 0.5 seconds before setting the dialogue panel inactive
+     */
     IEnumerator Wait()
     {
         yield return new WaitForSeconds(0.5f);
         dialoguePanel.SetActive(false);
     }
 
+    /*
+     * This method unfreezes the player, shows the character you are talking to on the field, sets textUpdated to false for the next
+     * time so it can update, and turns off TypeWriterEffect
+     */
     void SetToFalse()
     {
         UnfreezePlayer();
@@ -191,9 +240,43 @@ public class DialogueController : MonoBehaviour
 
         textUpdated = false;
         this.GetComponent<TypeWriterEffect>().updateText = false;
-        StopCoroutine(this.GetComponent<TypeWriterEffect>().ShowText());   
+        StopCoroutine(this.GetComponent<TypeWriterEffect>().ShowText());
     }
 
+    /*
+     * This method unfreezes the player, shows the character you are talking to on the field, sets textUpdated to false for the next
+     * time so it can update, and turns off TypeWriterEffect.
+     * ADDITIONALLY since this is only called when we encounter the special "SKIP" dialogue, it also sets the next name, dialogue, 
+     * and most importantly takes into account which sprite is going to show when we talk to the character again and sets the next sprite 
+     * to THAT one.
+     */
+    void SetToFalse2()
+    {
+        UnfreezePlayer();
+        ShowCharacter(); // show character on field
+
+        textUpdated = false;
+        this.GetComponent<TypeWriterEffect>().updateText = false;
+        StopCoroutine(this.GetComponent<TypeWriterEffect>().ShowText());
+
+        name.text = whoIsTalking[index]; // name of who is talking
+        this.GetComponent<TypeWriterEffect>().fullText = dialogue[index]; // set text in TypeWriterEffect to display
+        this.GetComponent<TypeWriterEffect>().updateText = true; // tell TypeWriterEffect to update the text
+        textUpdated = true; // set to true so that it does not keep updating over and over
+        
+        if (spriteOneActive)
+        {
+            sprite.sprite = dialogueSprites[index]; // set sprite 1 to the next sprite 
+        } else
+        {
+            sprite2.sprite = dialogueSprites[index]; // set sprite 1 to the first sprite
+        }
+        
+    }
+
+    /*
+     * This method fades in a sprite
+     */
     public IEnumerator FadeIn(Image sprite)
     {
         float start = 0f, end = 1f;
@@ -210,6 +293,9 @@ public class DialogueController : MonoBehaviour
         }
     }
 
+    /*
+     * This method fades out a sprite
+     */
     public IEnumerator FadeOut(Image sprite)
     {
         float start = 1f, end = 0f;
