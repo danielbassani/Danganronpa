fd2913c34c873a1e78c4074e58506a62f8ccf330
dantheman0124 <daniel.bassani@outlook.com>
2022-01-24 09:14:25 -0500

    add island demo scene

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,16 +8,30 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 10f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            speed = 16f;
+            speed = 14f;
         }else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             speed = 10f;
@@ -25,5 +39,9 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
